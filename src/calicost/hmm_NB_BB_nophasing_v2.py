d702e7d09d90e92c378176a25b0b7b7297bcc45b
Michael J. Wilson <michaeljwilson@princeton.edu>
2024-07-12 17:47:44 +0100

    fix

--- src/calicost/hmm_NB_BB_nophasing_v2.py
+++ src/calicost/hmm_NB_BB_nophasing_v2.py
@@ -228,14 +228,7 @@ class hmm_nophasing_v2(object):
         assert p_binom.shape == (n_states, n_spots)
 
         log_emission_baf = np.zeros((n_states, n_obs, n_spots))
-        """
-        for i in np.arange(n_states):
-            for s in np.arange(n_spots):
-                idx_nonzero_baf = np.where(total_bb_RD[:,s] > 0)[0]
-                
-                if len(idx_nonzero_baf) > 0:
-                    log_emission_baf[i, idx_nonzero_baf, s] = scipy.stats.betabinom.logpmf(X[idx_nonzero_baf,1,s], total_bb_RD[idx_nonzero_baf,s], p_binom[i, s] * taus[i, s], (1-p_binom[i, s]) * taus[i, s])
-        """
+
         if tumor_weight is None:
             tumor_weight = tumor_prop
         
