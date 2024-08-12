fc7025d534812c41689a913e1114e74dd661aae4
Michael J. Wilson <mw9568@princeton.edu>
2024-08-12 13:20:57 -0400

    add logging of emission fitting.

--- src/calicost/utils_distribution_fitting.py
+++ src/calicost/utils_distribution_fitting.py
@@ -75,7 +75,7 @@ class Weighted_NegativeBinomial(GenericLikelihoodModel):
 
         return -scipy.stats.nbinom.logpmf(self.endog, n, p).dot(self.weights)
 
-    def fit(self, start_params=None, maxiter=10000, maxfun=5000, **kwds):
+    def fit(self, start_params=None, maxiter=10_000, maxfun=5_000, **kwds):
         self.exog_names.append("alpha")
 
         if start_params is None:
@@ -112,33 +112,55 @@ class Weighted_NegativeBinomial(GenericLikelihoodModel):
 class Weighted_NegativeBinomial_mix(GenericLikelihoodModel):
     def __init__(self, endog, exog, weights, exposure, tumor_prop, seed=0, **kwds):
         super(Weighted_NegativeBinomial_mix, self).__init__(endog, exog, **kwds)
+
+        logger.info(f"Initializing Weighted_NegativeBinomial_mix model for endog.shape = {endog.shape}.")
+
         self.weights = weights
         self.exposure = exposure
         self.seed = seed
         self.tumor_prop = tumor_prop
 
-    #
     def nloglikeobs(self, params):
         nb_mean = self.exposure * (
             self.tumor_prop * np.exp(self.exog @ params[:-1]) + 1 - self.tumor_prop
         )
         nb_std = np.sqrt(nb_mean + params[-1] * nb_mean**2)
-        n, p = convert_params(nb_mean, nb_std)
-        llf = scipy.stats.nbinom.logpmf(self.endog, n, p)
-        neg_sum_llf = -llf.dot(self.weights)
-        return neg_sum_llf
 
-    #
-    def fit(self, start_params=None, maxiter=10000, maxfun=5000, **kwds):
+        n, p = convert_params(nb_mean, nb_std)
+
+        return -scipy.stats.nbinom.logpmf(self.endog, n, p).dot(self.weights)
+
+    def fit(self, start_params=None, maxiter=10_000, maxfun=5_000, **kwds):
         self.exog_names.append("alpha")
+
         if start_params is None:
             if hasattr(self, "start_params"):
                 start_params = self.start_params
             else:
                 start_params = np.append(0.1 * np.ones(self.nparams), 0.01)
-        return super(Weighted_NegativeBinomial_mix, self).fit(
-            start_params=start_params, maxiter=maxiter, maxfun=maxfun, **kwds
+
+        logger.info(f"Starting Weighted_NegativeBinomial_mix optimization with start_params = {start_params}.")
+
+        start = time.time()
+
+        result = super(Weighted_NegativeBinomial_mix, self).fit(
+            start_params=start_params,
+            maxiter=maxiter,
+            maxfun=maxfun,
+            disp=False,
+            skip_hessian=True,
+            callback=None,
+            full_output=True,
+            retall=False,
+            **kwds
         )
+
+        # NB specific to nm (Nelder-Mead) optimization.
+        niter = result.mle_retvals["iterations"]       
+
+        logger.info(f"Finished Weighted_NegativeBinomial_mix optimization in {time.time() - start:.2f} seconds, with {niter} iterations.")
+
+        return result
 
 
 class Weighted_BetaBinom(GenericLikelihoodModel):
@@ -160,23 +182,23 @@ class Weighted_BetaBinom(GenericLikelihoodModel):
     exposure : array, (n_samples,)
         Total number of trials. In BAF case, this is the total number of SNP-covering UMIs.
     """
-
     def __init__(self, endog, exog, weights, exposure, **kwds):
         super(Weighted_BetaBinom, self).__init__(endog, exog, **kwds)
+
+        logger.info(f"Initializing Weighted_BetaBinomial model for endog.shape = {endog.shape}.")
+
         self.weights = weights
         self.exposure = exposure
 
-    #
     def nloglikeobs(self, params):
         a = (self.exog @ params[:-1]) * params[-1]
         b = (1 - self.exog @ params[:-1]) * params[-1]
-        llf = scipy.stats.betabinom.logpmf(self.endog, self.exposure, a, b)
-        neg_sum_llf = -llf.dot(self.weights)
-        return neg_sum_llf
 
-    #
-    def fit(self, start_params=None, maxiter=10000, maxfun=5000, **kwds):
+        return -scipy.stats.betabinom.logpmf(self.endog, self.exposure, a, b).dot(self.weights)
+
+    def fit(self, start_params=None, maxiter=10_000, maxfun=5_000, **kwds):
         self.exog_names.append("tau")
+
         if start_params is None:
             if hasattr(self, "start_params"):
                 start_params = self.start_params
@@ -184,34 +206,55 @@ class Weighted_BetaBinom(GenericLikelihoodModel):
                 start_params = np.append(
                     0.5 / np.sum(self.exog.shape[1]) * np.ones(self.nparams), 1
                 )
-        return super(Weighted_BetaBinom, self).fit(
-            start_params=start_params, maxiter=maxiter, maxfun=maxfun, **kwds
+
+        logger.info(f"Starting Weighted_BetaBinomial optimization with start_params = {start_params}.")
+
+        start = time.time()
+
+        result = super(Weighted_BetaBinom, self).fit(
+            start_params=start_params,
+            maxiter=maxiter,
+            maxfun=maxfun,
+            disp=False,
+            skip_hessian=True,
+            callback=None,
+            full_output=True,
+            retall=False,
+            **kwds
         )
 
+        # NB specific to nm (Nelder-Mead) optimization.
+        niter = result.mle_retvals["iterations"]
+
+        logger.info(f"Finished Weighted_BetaBinomial optimization in {time.time() - start:.2f} seconds, with {niter} iterations.")
+
+        return result
 
 class Weighted_BetaBinom_mix(GenericLikelihoodModel):
     def __init__(self, endog, exog, weights, exposure, tumor_prop, **kwds):
         super(Weighted_BetaBinom_mix, self).__init__(endog, exog, **kwds)
+
+        logger.info(f"Initializing Weighted_BetaBinom_mix model for endog.shape = {endog.shape}.")
+
         self.weights = weights
         self.exposure = exposure
         self.tumor_prop = tumor_prop
 
-    #
     def nloglikeobs(self, params):
         a = (
             self.exog @ params[:-1] * self.tumor_prop + 0.5 * (1 - self.tumor_prop)
         ) * params[-1]
+
         b = (
             (1 - self.exog @ params[:-1]) * self.tumor_prop
             + 0.5 * (1 - self.tumor_prop)
         ) * params[-1]
-        llf = scipy.stats.betabinom.logpmf(self.endog, self.exposure, a, b)
-        neg_sum_llf = -llf.dot(self.weights)
-        return neg_sum_llf
 
-    #
-    def fit(self, start_params=None, maxiter=10000, maxfun=5000, **kwds):
+        return -scipy.stats.betabinom.logpmf(self.endog, self.exposure, a, b).dot(self.weights)
+
+    def fit(self, start_params=None, maxiter=10_000, maxfun=5_000, **kwds):
         self.exog_names.append("tau")
+
         if start_params is None:
             if hasattr(self, "start_params"):
                 start_params = self.start_params
@@ -219,9 +262,29 @@ class Weighted_BetaBinom_mix(GenericLikelihoodModel):
                 start_params = np.append(
                     0.5 / np.sum(self.exog.shape[1]) * np.ones(self.nparams), 1
                 )
-        return super(Weighted_BetaBinom_mix, self).fit(
-            start_params=start_params, maxiter=maxiter, maxfun=maxfun, **kwds
+
+        logger.info(f"Starting Weighted_BetaBinom_mix optimization with start_params = {start_params}.")
+
+        start = time.time()
+
+        result = super(Weighted_BetaBinom_mix, self).fit(
+            start_params=start_params,
+            maxiter=maxiter,
+            maxfun=maxfun,
+            disp=False,
+            skip_hessian=True,
+            callback=None,
+            full_output=True,
+            retall=False,
+            **kwds
         )
+
+        # NB specific to nm (Nelder-Mead) optimization.
+        niter = result.mle_retvals["iterations"]
+
+        logger.info(f"Finished Weighted_BetaBinom_mix optimization in {time.time() - start:.2f} seconds, with {niter} iterations.")
+
+        return result
 
 
 class Weighted_BetaBinom_fixdispersion(GenericLikelihoodModel):
