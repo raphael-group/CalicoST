ecfff3533a05f7ee4c628d9f0920d5b18eca1c33
Michael J. Wilson <mw9568@princeton.edu>
2024-08-15 10:48:11 -0400

    revert imports for hmrf

--- src/calicost/hmrf.py
+++ src/calicost/hmrf.py
@@ -1,26 +1,25 @@
-import copy
 import logging
-import warnings
-from pathlib import Path
-# from turtle import reset
-
-# import networkx as nx
+from turtle import reset
 import numpy as np
 import pandas as pd
-import scipy.sparse
-import scipy.special
 from numba import njit
+import scipy.special
+import scipy.sparse
+from sklearn.mixture import GaussianMixture
 from sklearn.cluster import KMeans
 from sklearn.metrics import adjusted_rand_score, silhouette_score
-from sklearn.mixture import GaussianMixture
 from sklearn.neighbors import kneighbors_graph
-from statsmodels.tools.sm_exceptions import ValueWarning
+import networkx as nx
 from tqdm import trange
-
+import copy
+from pathlib import Path
 from calicost.hmm_NB_BB_phaseswitch import *
 from calicost.utils_distribution_fitting import *
-from calicost.utils_hmrf import *
 from calicost.utils_IO import *
+from calicost.utils_hmrf import *
+
+import warnings
+from statsmodels.tools.sm_exceptions import ValueWarning
 
 logger = logging.getLogger(__name__)
 
