cecf7028342c20fd6f4ca835fdb2fca1db5ec9ba
Michael J. Wilson <mw9568@princeton.edu>
2024-08-18 18:59:07 -0400

    add TODO for gammas in phasing beta binomial

--- src/calicost/hmm_NB_BB_phaseswitch.py
+++ src/calicost/hmm_NB_BB_phaseswitch.py
@@ -80,6 +80,7 @@ class hmm_sitewise(object):
 
         log_emission_rdr = np.zeros((2 * n_states, n_obs, n_spots))
         log_emission_baf = np.zeros((2 * n_states, n_obs, n_spots))
+        
         for i in np.arange(n_states):
             for s in np.arange(n_spots):
                 # expression from NB distribution
@@ -94,22 +95,28 @@ class hmm_sitewise(object):
                     log_emission_rdr[i + n_states, idx_nonzero_rdr, s] = (
                         log_emission_rdr[i, idx_nonzero_rdr, s]
                     )
+                    
                 # AF from BetaBinom distribution
                 idx_nonzero_baf = np.where(total_bb_RD[:, s] > 0)[0]
+                
                 if len(idx_nonzero_baf) > 0:
                     log_emission_baf[i, idx_nonzero_baf, s] = (
                         scipy.stats.betabinom.logpmf(
                             X[idx_nonzero_baf, 1, s],
                             total_bb_RD[idx_nonzero_baf, s],
                             p_binom[i, s] * taus[i, s],
-                            (1 - p_binom[i, s]) * taus[i, s],
+                            (1. - p_binom[i, s]) * taus[i, s],
                         )
                     )
+
+                    # TODO 
+                    # log_emission_baf[i, idx_nonzero_baf, s] - scipy.special.loggamma(X[idx_nonzero_baf, 1, s] + p_binom[i, s] * taus[i, s]) - scipy.special.loggamma(total_bb_RD[idx_nonzero_baf, s] - X[idx_nonzero_baf, 1, s] + (1. - p_binom[i, s]) * taus[i, s]) + scipy.special.loggamma(X[idx_nonzero_baf, 1, s] + (1. - p_binom[i, s]) * taus[i, s]) + scipy.special.loggamma(total_bb_RD[idx_nonzero_baf, s] - X[idx_nonzero_baf, 1, s] + p_binom[i, s] * taus[i, s])
+                    
                     log_emission_baf[i + n_states, idx_nonzero_baf, s] = (
                         scipy.stats.betabinom.logpmf(
                             X[idx_nonzero_baf, 1, s],
                             total_bb_RD[idx_nonzero_baf, s],
-                            (1 - p_binom[i, s]) * taus[i, s],
+                            (1. - p_binom[i, s]) * taus[i, s],
                             p_binom[i, s] * taus[i, s],
                         )
                     )
