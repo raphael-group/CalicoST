eacbf99ab5547e504c18117cf9b9d54948f1e1e8
Cong Ma <congma@Congs-MacBook-Air.local>
2025-06-06 12:13:17 -0400

    plotting function that distinguishes normal_ref and clone 0

--- src/calicost/utils_plotting.py
+++ src/calicost/utils_plotting.py
@@ -1495,12 +1495,56 @@ def plot_individual_spots_in_space(coords, assignment, single_tumor_prop=None, s
     
     fig, axes = plt.subplots(1, 1, figsize=(base_width*n_samples, base_height), dpi=200, facecolor="white")
     if "clone 0" in final_clone_ids:
-        colorlist = ['lightgrey'] + seaborn.color_palette("Set2", n_final_clones-1).as_hex()
+        colorlist = ['lightgrey'] + seaborn.color_palette(palette, n_final_clones-1).as_hex()
     else:
-        colorlist = seaborn.color_palette("Set2", n_final_clones).as_hex()
+        colorlist = seaborn.color_palette(palette, n_final_clones).as_hex()
 
     for c,cid in enumerate(final_clone_ids):
         idx = np.where( (assignment.values==cid) )[0]
+        if single_tumor_prop is None:
+            seaborn.scatterplot(x=shifted_coords[idx,0], y=-shifted_coords[idx,1], s=10, color=colorlist[c], linewidth=0, legend=None, ax=axes)
+        else:
+            # cmap
+            this_full_cmap = seaborn.color_palette(f"blend:lightgrey,{colorlist[c]}", as_cmap=True)
+            quantile_colors = this_full_cmap(np.array([0, np.min(copy_single_tumor_prop[idx]), np.max(copy_single_tumor_prop[idx]), 1]))
+            quantile_colors = [matplotlib.colors.rgb2hex(x) for x in quantile_colors[1:-1]]
+            this_cmap = seaborn.color_palette(f"blend:{quantile_colors[0]},{quantile_colors[-1]}", as_cmap=True)
+            seaborn.scatterplot(x=shifted_coords[idx,0], y=-shifted_coords[idx,1], s=10, hue=copy_single_tumor_prop[idx], palette=this_cmap, linewidth=0, legend=None, ax=axes)
+
+    legend_elements = [Line2D([0], [0], marker='o', color="w", markerfacecolor=colorlist[c], label=cid, markersize=10) for c,cid in enumerate(final_clone_ids)]
+    axes.legend(legend_elements, final_clone_ids, handlelength=0.1, loc="upper left", bbox_to_anchor=(1,1))
+    axes.axis("off")
+
+    fig.tight_layout()
+    return fig
+
+
+def plot_individual_spots_in_space_categorical(coords, assignment, single_tumor_prop=None, sample_list=None, sample_ids=None, base_width=4, base_height=3, palette="Set2", add_grey=True):
+    # combine coordinates across samples
+    shifted_coords = copy.copy(coords)
+    if not (sample_ids is None):
+        x_offset = 0
+        for s,sname in enumerate(sample_list):
+            index = np.where(sample_ids == s)[0]
+            shifted_coords[index,0] = shifted_coords[index,0] + x_offset
+            x_offset += np.max(coords[index,0]) + 10
+
+    # number of clones and samples
+    final_clone_ids = assignment.categories
+    n_final_clones = len(final_clone_ids)
+    n_samples = 1 if sample_list is None else len(sample_list)
+
+    # remove nan of single_tumor_prop
+    if not single_tumor_prop is None:
+        copy_single_tumor_prop = copy.copy(single_tumor_prop)
+        copy_single_tumor_prop[np.isnan(copy_single_tumor_prop)] = 0.5
+    
+    fig, axes = plt.subplots(1, 1, figsize=(base_width*n_samples, base_height), dpi=200, facecolor="white")
+    if add_grey:
+        colorlist = ['lightgrey'] + seaborn.color_palette(palette, n_final_clones-1).as_hex()
+
+    for c,cid in enumerate(final_clone_ids):
+        idx = np.where( (assignment==cid) )[0]
         if single_tumor_prop is None:
             seaborn.scatterplot(x=shifted_coords[idx,0], y=-shifted_coords[idx,1], s=10, color=colorlist[c], linewidth=0, legend=None, ax=axes)
         else:
